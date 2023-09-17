0b55d012cd24dcae8b25f10fd990a9df20b486dc
Mikhail <babichev.mikhail.s@gmail.com>
2023-09-17 13:00:47 +0700

    Initial commit

---
+++ lab1/MatrixMethods.cs
@@ -0,0 +1,176 @@
+﻿namespace lab1;
+using static Int32;
+
+public static class MatrixMethods
+{
+    public static void MultiplyMatrix(List<List<int>> matrix, List<List<int>> anotherMatrix, ref int[] to, int count,
+        ref List<List<int>> matrixToSum, ref int something)
+    {
+        var matrixDimension = matrix.Count;
+        
+        var newMatrix = new List<List<int>>();
+
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            if (anotherMatrix.Sum(line => line[i]) == 0 && to[i] == -1)
+            {
+                to[i] = count;
+            }
+            var line = new List<int>();
+            for (var j = 0; j < matrixDimension; j++)
+            {
+                var sum = 0;
+                var firstElements = matrix[i];
+                var secondElements = anotherMatrix.Select(l => l[j]).ToList();
+                for (var k = 0; k < matrixDimension; k++)
+                {
+                    sum += firstElements[k] * secondElements[k];
+                }
+                line.Add(sum);
+            }
+            
+            newMatrix.Add(line);
+        }
+        
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            for (var j = 0; j < matrixDimension; j++)
+            {
+                matrixToSum[i][j] += newMatrix[i][j];
+            }
+        }
+
+        count++;
+        something = count;
+        WriteMatrix(newMatrix);
+
+        if (newMatrix.Any(l => l.Any(value => value != 0)))
+        {
+            Console.WriteLine();
+            MultiplyMatrix(matrix, newMatrix, ref to, count, ref matrixToSum, ref something);
+        }
+        else
+        {
+            for (var i = 0; i < to.Length; i++)
+            {
+                if (to[i] == -1)
+                {
+                    to[i] = count;
+                }
+            }
+        }
+    }
+
+    public static void WriteMatrix(List<List<int>> matrix)
+    {
+        var matrixDimension = matrix.Count;
+        var sums = new List<int>();
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            sums.Add(matrix.Sum(line => line[i]));
+            
+            Console.Write("|");
+            for (var j = 0; j < matrixDimension; j++)
+            {
+                Console.Write($" {matrix[i][j]} |");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            var sum = matrix[i].Sum();
+            Console.Write($" {sum}");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("|");
+        foreach (var sum in sums)
+        {
+            Console.Write($" {sum} |");
+        }
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+    
+    public static void CheckMatrix(List<List<int>> matrix)
+    {
+        var matrixDimension = matrix.Count;
+
+        var ways = new List<int>();
+
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            for (var j = 0; j < matrixDimension; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    ways.Add(j);
+                    CheckElement(j, matrixDimension, matrix, ref ways);
+                    ways.Clear();
+                }
+            }
+        }
+        
+        Console.WriteLine("Исходная матрица корректна");
+    }
+
+    public static List<List<int>> FillMatrix(int matrixDimension)
+    {
+        var matrix = new List<List<int>>();
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            var line = new List<int>();
+            int? value = null;
+    
+            for (var j = 0; j < matrixDimension; j++)
+            {
+                Console.Write($"Введите значение элемента [{i};{j}]: ");
+                if (TryParse(Console.ReadLine(), out var convertedValue))
+                {
+                    value = convertedValue;
+                }
+        
+                ArgumentNullException.ThrowIfNull(value);
+
+                if (value != 0 && value != 1)
+                {
+                    throw new Exception("Значения в матрице могут быть только 0 или 1");
+                }
+        
+                line.Add(value.Value);
+        
+                Console.WriteLine();
+            }
+    
+            matrix.Add(line);
+        }
+
+        return matrix;
+    }
+
+    public static void AddLine(string? text = null)
+    {
+        if (text is null)
+        {
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine(text);
+    }
+    
+    private static void CheckElement(int rowNumber, int matrixDimension, List<List<int>> matrix, ref List<int> ways)
+    {
+        for (var i = 0; i < matrixDimension; i++)
+        {
+            if (matrix[rowNumber][i] == 1)
+            {
+                if (ways.Contains(i))
+                {
+                    throw new Exception("Граф, построенный по исходной матрице закольцован");
+                }
+                
+                ways.Add(i);
+                CheckElement(i, matrixDimension, matrix, ref ways);
+            }
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,145 @@
+﻿using lab1;
+using static System.Int32;
+
+// Ввод размерности матрицы
+Console.Write("Введите размерность матрицы: ");
+int? matrixDimension = null;
+
+// Ковертируем размерность в число
+if (TryParse(Console.ReadLine(), out var convertedDimension))
+{
+    matrixDimension = convertedDimension;
+}
+
+// Если не получилось сконвертировать кидаем ошибку
+ArgumentNullException.ThrowIfNull(matrixDimension);
+
+// Если размерность матрицы меньше или равна нулю
+if (matrixDimension <= 0)
+{
+    throw new Exception("Размерность матрицы должна быть положительным целым числом");
+}
+
+MatrixMethods.AddLine();
+
+// Заполнение матрицы пользователем
+var matrix = MatrixMethods.FillMatrix(matrixDimension.Value);
+
+MatrixMethods.AddLine("Исходная матрица:");
+
+// Вывод матрицы на экран
+MatrixMethods.WriteMatrix(matrix);
+
+Console.WriteLine();
+Console.Write("Выходные элементы: ");
+for (var i = 0; i < matrixDimension; i++)
+{
+    if (matrix[i].Sum() == 0)
+    {
+        Console.Write($"{i + 1} ");
+    }
+}
+
+Console.WriteLine();
+Console.Write("Входные элементы: ");
+for (var i = 0; i < matrixDimension; i++)
+{
+    if (matrix.Select(list => list[i]).Sum() == 0)
+    {
+        Console.Write($"{i + 1} ");
+    }
+}
+Console.WriteLine();
+// Проверка на зацикленность
+//MatrixMethods.CheckMatrix(matrix);
+
+// Такты образования
+var to = Enumerable.Repeat(-1, matrixDimension.Value).ToArray();
+
+// Матрица прямых и косвенных связей
+var matrixToSum = new List<List<int>>();
+
+// Заполнение матрицы нулями
+for (var i = 0; i < matrixDimension; i++)
+{
+    var list = new List<int>();
+    matrixToSum.Add(FillWithZeros(list, matrixDimension.Value));
+}
+
+var something = 0;
+
+// Умножение матрицы на саму себя + такты образования + матрица прямых и косвенных связей
+MatrixMethods.MultiplyMatrix(matrix, matrix, ref to, 0, ref matrixToSum, ref something);
+
+Console.WriteLine();
+Console.WriteLine($"Порядок графа: {something - 1}");
+Console.WriteLine();
+
+// Такты гашения
+var tg = Enumerable.Repeat(0, matrixDimension.Value).ToArray();
+
+// Заполнение тактов гашения
+for (var i = 0; i < matrixDimension; i++)
+{
+    var max = 0;
+    for (var j = 0; j < matrixDimension; j++)
+    {
+        max = to[j] * matrix[i][j] > max
+            ? to[j] * matrix[i][j]
+            : max;
+    }
+
+    tg[i] = max;
+    
+    if (to[i] > tg[i])
+    {
+        tg[i] = to[i];
+    }
+}
+
+// суммируем матрицу прямых и косвенных связей с исходной матрицей
+for (var i = 0; i < matrixDimension; i++)
+{
+    for (var j = 0; j < matrixDimension; j++)
+    {
+        matrixToSum[i][j] += matrix[i][j];
+    }
+}
+
+Console.WriteLine();
+// Вывод тактов образования
+Console.Write("To |");
+foreach (var item in to)
+{
+    Console.Write($"{ item} |");
+}
+
+Console.WriteLine();
+// Вывод тактов гашения
+Console.Write("Tг |");
+foreach (var item in tg)
+{
+    Console.Write($"{ item} |");
+}
+
+Console.WriteLine();
+Console.Write("Tх |");
+for (var i = 0; i < matrixDimension; i++)
+{
+    Console.Write($"{ tg[i] - to[i]} |");
+}
+
+MatrixMethods.AddLine();
+
+// Вывод матрицы прмых и косвенных связей
+MatrixMethods.WriteMatrix(matrixToSum);
+
+List<int> FillWithZeros(List<int> list, int dimension)
+{
+    for (var i = 0; i < dimension; i++)
+    {
+        list.Add(0);
+    }
+
+    return list;
+}
